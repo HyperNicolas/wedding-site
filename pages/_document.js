832a1689df55f1fd56277e7681741f5a8075b20b
Nicolas Jonnaert <nicolas@hypercharge.io>
2021-07-25 17:10:52 +0200

    added ffav icon

--- pages/_document.js
+++ pages/_document.js
@@ -31,6 +31,31 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="nl">
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/static/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/static/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/static/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/static/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/static/safari-pinned-tab.svg"
+          color="#9caaa7"
+        />
+        <meta name="msapplication-TileColor" content="#9caaa7" />
+        <meta name="theme-color" content="#ffffff" />
         <Head>
           <link
             rel="stylesheet"
